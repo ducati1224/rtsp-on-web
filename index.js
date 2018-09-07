df60fceeab7657275a83caf36ba23c09d528e276
Benjamin Wang <crocodile00323@gmail.com>
2018-09-07 14:59:56 +0800

    add html title

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.get("/", handleRtsp, function(req, res){
     var message = '';
     if (options.live === 'true') {
         message = `Live on server ${options.s} channel ${options.c.slice(2)}`
+    } else if (options.live === 'false') {
+        message = `Playback on server ${options.s} channel ${options.c.slice(2)} from ${options.start.slice(0, 4)}/${options.start.slice(4, 6)}/${options.start.slice(6, 8)} ${options.start.slice(9, 11)}:${options.start.slice(11, 13)}:${options.start.slice(13)} to ${options.end.slice(0, 4)}/${options.end.slice(4, 6)}/${options.end.slice(6, 8)} ${options.end.slice(9, 11)}:${options.end.slice(11, 13)}:${options.end.slice(13)}`;
     }
     res.render('index', {message});
 });
